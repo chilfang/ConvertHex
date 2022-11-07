7cf4e379cb29cb7e6ac4e61e0d6fc98ad8f0e939
Ian Evit <Administrator@MININT-EEJ88UO>
2022-11-07 13:24:17 -0700

    Add ConvertToHex() function

--- ConvertHex/Program.cs
+++ ConvertHex/Program.cs
@@ -3,6 +3,17 @@
 namespace ConvertHex {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine(ConvertToHex("hello world"));
+        }
+
+        public static string ConvertToHex(string input) {
+            string output = "";
+
+            foreach (char character in input) {
+                output += new string(((int) character).ToString("x")) + " ";
+            }
+
+            return output.Trim();
         }
     }
 }
